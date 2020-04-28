7d74d82b8e17250c879cfe41191ff3058c762427
DefiantHero <60482802+DefiantHero@users.noreply.github.com>
2020-04-28 14:16:31 -0400

    Add files via upload

---
+++ Data/RecipeHolderContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeHolder.Models
+{
+    public class RecipeHolderContext : DbContext
+    {
+        public RecipeHolderContext (DbContextOptions<RecipeHolderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RecipeHolder.Models.Recipe> Recipe { get; set; }
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeHolder.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Display(Name ="RecipeName")]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters")]
+        public string Name { get; set; }
+        [Required]
+        public string Instructions { get; set; }
+        [Required]
+        [Display(Name="Category of Food")]
+        public string Category { get; set; }
+        [Required]
+        [Range (0,1000)]
+        public int Servings { get; set; }
+    }
+}
